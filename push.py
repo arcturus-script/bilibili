2d2eef899e1059a47f0bf49979df39e32827a723
ARCTURUS <1638330246@qq.com>
2021-12-25 18:47:49 +0800

    修复pushplus推送失败的问题

--- push.py
+++ push.py
@@ -99,7 +99,7 @@ class pushplus():
             'content': content,
             "template": "markdown"
         }
-        rep = requests.post(url, params=params)
+        rep = requests.post(url, params=params).json()
         if rep["code"] == 200:
             print("发送消息成功")
         else:
